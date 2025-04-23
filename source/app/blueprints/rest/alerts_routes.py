b1fd0e9d7e35753a92792957dde0cd5545e870bd
Elise17 <elise17web@gmail.com>
2025-04-23 11:18:59 +0200

    Started implementation of PUT /api/v2/alerts/{identifier}

--- source/app/blueprints/rest/alerts_routes.py
+++ source/app/blueprints/rest/alerts_routes.py
@@ -275,6 +275,7 @@ def alerts_similarities_route(alert_id) -> Response:
 
 
 @alerts_rest_blueprint.route('/alerts/update/<int:alert_id>', methods=['POST'])
+@endpoint_deprecated('PUT', '/api/v2/alerts')
 @ac_api_requires(Permissions.alerts_write)
 def alerts_update_route(alert_id) -> Response:
     """
