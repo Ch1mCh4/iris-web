9c69dd8c016ddc1176eb2547a9a4eae5a633011c
Elise17 <elise17web@gmail.com>
2025-04-25 14:19:57 +0200

    Added new test_update_alert_should_return_200_when_alert_title_change

--- tests/tests_rest_alerts.py
+++ tests/tests_rest_alerts.py
@@ -170,6 +170,22 @@ class TestsRestAlerts(TestCase):
         identifier = response['data']['alert_id']
         response = self._subject.update(f'/api/v2/alerts/{identifier}', {})
         self.assertEqual(200, response.status_code)
+
+    def test_update_alert_should_return_200_when_alert_title_change(self):
+        alert_title = f'title{uuid4()}'
+        new_alert_title = "test_new_title"
+        body = {
+            'alert_title': alert_title,
+            'alert_severity_id': 4,
+            'alert_status_id': 3,
+            'alert_customer_id': 1
+        }
+        response = self._subject.create('/alerts/add', body).json()
+        identifier = response['data']['alert_id']
+        body = {'alert_title': new_alert_title}
+        response = self._subject.update(f'/api/v2/alerts/{identifier}',body).json()
+        new_title = response['alert_title']
+        self.assertEqual(new_title, new_alert_title)
     
     def test_update_alert_should_return_403_when_user_has_no_permission_to_alert(self):
         user = self._subject.create_dummy_user()
